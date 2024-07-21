fe9ee556f7c0fb440241ef62e686a86475ec1b6b
katiandri <aleksanders938@gmail.com>
2024-07-21 20:10:17 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE = "https://b7c83bd8-fc63-4a41-905d-5656045a8300.serverhub.praktikum-services.ru/"
+CREAT_ORDERS = "/api/v1/orders/"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Rick",
+    "lastName": "Sanchez",
+    "address": "Dimension C-137, 6910 apt.",
+    "metroStation": 13,
+    "phone": "+7 800 012 03 04",
+    "rentTime": 3,
+    "deliveryDate": "2020-07-23",
+    "comment": "Wubba Lubba Dub-Dub!",
+    "color": [
+        "BLACK"
+    ]
+}
